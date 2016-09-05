b596b79b1c1abfe490f8b6659ed29b15a78ff81b
qinc2149 <964235895@qq.com>
2016-09-05 10:53:45 +0800

    实验了一下SourceTree

---
+++ src/ProduceAndConsume/ThreadJoin.java
@@ -0,0 +1,33 @@
+package ProduceAndConsume;
+/**
+ * @author Qinc
+ * 演示join
+ * 没写完,试一下
+ *
+ */
+
+public class ThreadJoin {
+	public static void main(String[] args) {
+		task tk = new task();
+		Thread t1= new Thread(tk);
+		t1.start();
+		//Thread.
+		System.out.println(Thread.currentThread()+"主线程在执行结束！");
+	}	
+}
+class task implements Runnable{
+
+	public void run() {
+		for(int i=0;i<10;i++){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread()+"子线程在执行；");
+		}
+		
+	}
+	
+}
